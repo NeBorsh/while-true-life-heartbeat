6d4c6831edc913902e35c93143a9287760801bff
Borsh <85323386+NeBorsh@users.noreply.github.com>
2023-10-25 03:09:24 +0300

    ❤

--- main.cpp
+++ main.cpp
@@ -6,14 +6,14 @@ using namespace std;
 void life() {
   while (true) {
     cout << ":3" << endl;
-    std::this_thread::sleep_for(std::chrono::seconds(1));
+    this_thread::sleep_for(chrono::seconds(1));
   }
 }
 
 void heartbeat() {
   while (true) {
     cout << "heartbeat" << endl;
-    std::this_thread::sleep_for(std::chrono::seconds(1));
+    this_thread::sleep_for(chrono::seconds(1));
   }
 }
 
